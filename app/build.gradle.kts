f4343a816c688e4675390f026ceb305fc64b4b08
TM <tjmcard@gmail.com>
2024-01-02 10:58:50 -0800

    Google SSO Basic Implementation - Call and Response

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.abyxcz.googlesso"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.abyxcz.googlesso"
@@ -66,4 +66,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //Google SSO
+    implementation("com.google.android.gms:play-services-auth:19.0.0")
 }
--- app/src/main/java/com/abyxcz/googlesso/MainActivity.kt
+++ app/src/main/java/com/abyxcz/googlesso/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.abyxcz.googlesso
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,8 +13,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.abyxcz.googlesso.ui.theme.GoogleSSOTheme
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 
 class MainActivity : ComponentActivity() {
+
+    private val RC_SIGN_IN = 1234
+    private val GOOGLE_SSO_TOKEN = "your_sign_in_token"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -24,6 +35,37 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Greeting("Android")
                 }
+            }
+        }
+
+        initGoogleSSO()
+    }
+
+
+    fun initGoogleSSO(){
+
+        val mSignInClient: GoogleSignInClient
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(GOOGLE_SSO_TOKEN)
+            .requestEmail()
+            .build()
+        mSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val signInIntent: Intent = mSignInClient.getSignInIntent()
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            println("$resultCode $data")
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account: GoogleSignInAccount? = task.getResult(ApiException::class.java)
+                /* continue with account */
+            } catch (e: ApiException) {
+                Log.w("MainActivity", "Google sign in failed", e)
             }
         }
     }
